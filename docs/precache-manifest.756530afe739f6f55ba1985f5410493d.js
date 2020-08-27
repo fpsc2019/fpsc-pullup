182412dcab8f4d2b53bb39a515cc09e5706c3137
fpsc2020 <fpsc2020@fpsc>
2020-08-27 10:18:45 +0800

    update

---
+++ docs/precache-manifest.756530afe739f6f55ba1985f5410493d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00dcccebd5ee824527a32d47b342b75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d278fa2d1ef05fb18dae",
+    "url": "./static/css/2.633bb5e4.chunk.css"
+  },
+  {
+    "revision": "ca09b4c6b19eab09bae8",
+    "url": "./static/css/main.5d737727.chunk.css"
+  },
+  {
+    "revision": "d278fa2d1ef05fb18dae",
+    "url": "./static/js/2.c5ea1a60.chunk.js"
+  },
+  {
+    "revision": "ca09b4c6b19eab09bae8",
+    "url": "./static/js/main.5e15e418.chunk.js"
+  },
+  {
+    "revision": "2436e15b78e239d3e943",
+    "url": "./static/js/runtime~main.d3da9e8f.js"
+  },
+  {
+    "revision": "af3a347311df3ce4f0bb41f11dd922db",
+    "url": "./static/media/logo.af3a3473.png"
+  }
+]);
